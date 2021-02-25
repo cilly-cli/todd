3852d4e2ec9ee98d370e1804c46d634da2076970
Anders Brams <abra@ambolt.io>
2021-02-25 22:54:06 +0100

    Added presentation

--- src/commands/deploy/github/github.ts
+++ src/commands/deploy/github/github.ts
@@ -1,10 +1,28 @@
-import { CliCommand } from 'cilly'
+import { CliCommand, Option } from 'cilly'
+import { say } from '../../../presentation'
 import { DeployOptions } from '../deploy'
 
-export type DeployGitHubOptions = DeployOptions
+interface DeployGitHubOptions extends DeployOptions {
+  version: string
+  title: string
+  description: string
+  branch: string
+  repo: string
+  accessToken: string
+}
+
+const options: Option[] = [
+  { name: ['-v', '--version'], description: 'The release version (e.g. v1.0.0) ', required: true, args: [{ name: 'version', required: true }] },
+  { name: ['-t', '--title'], description: 'The release title (e.g. Release v1.0.0) ', required: true, args: [{ name: 'title', required: true }] },
+  { name: ['-c', '--changelog'], description: 'Path to the changelog file', args: [{ name: 'path' }] },
+  { name: ['-r', '--repo'], description: 'The repository name ', required: true, args: [{ name: 'repo', required: true }] },
+  { name: ['-b', '--branch'], description: 'The branch name to create a release from', defaultValue: 'main', args: [{ name: 'branch' }] },
+  { name: ['-a', '--access-token'], description: 'The GitHub access token ', required: true, args: [{ name: 'token', required: true }] }
+]
 
 export const github = new CliCommand('github', { inheritOpts: true })
   .withDescription('Create a GitHub release with your program as an asset')
-  .withHandler((args, opts: DeployOptions) => {
-    github.help()
+  .withOptions(...options)
+  .withHandler((args, opts: DeployGitHubOptions) => {
+    say(`Creating Release-${opts.title}...`)
   })
---
+++ src/presentation.ts
@@ -0,0 +1,48 @@
+import chalk from 'chalk'
+
+const colors = {
+  info: '#6EC1E4',
+  success: '#26C485',
+  warn: '#F7B538',
+  error: '#D8315B',
+  muted: '#8A8A8A',
+}
+
+export const info = (msg: string): string => {
+  return chalk.hex(colors.info).bold(msg)
+}
+
+export const success = (msg: string): string => {
+  return chalk.hex(colors.success).bold(msg)
+}
+
+export const warn = (msg: string): string => {
+  return chalk.hex(colors.warn).bold(msg)
+}
+
+export const error = (msg: string): string => {
+  return chalk.hex(colors.error).bold(msg)
+}
+
+export const muted = (msg: string): string => {
+  return chalk.dim(msg)
+}
+
+export const underlined = (msg: string): string => {
+  return chalk.underline(msg)
+}
+
+export const cyan = chalk.hex(colors.info)
+export const green = chalk.hex(colors.success)
+export const yellow = chalk.hex(colors.warn)
+export const red = chalk.hex(colors.error)
+
+export const icons = {
+  check: process.platform == 'win32' ? '√' : '✔',
+  cross: process.platform == 'win32' ? '×' : '✖',
+  enter: process.platform == 'win32' ? 'ENTER' : '⎆'
+}
+
+export const say = (msg: string): void => {
+  console.log(`${info('Todd:')} ${msg}`)
+}
